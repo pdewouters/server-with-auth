a7b695d2564c9c3b50ad5db1ed1849853067886e
Paul de Wouters <paul@hmn.md>
2016-05-07 16:49:54 +0100

    port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors())
 app.use(bodyParser.json({ type: '*/*' }))
 router(app)
 // Server setup
-const port = process.env.PORT || 3090
+const port = process.env.PORT || 5000
 const server = http.createServer(app)
 server.listen(port)
 console.log('Server listening on: ', port)
