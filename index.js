7bde529001774ed7a3db9bdfee480450181da23c
Paul de Wouters <paul@hmn.md>
2016-05-14 14:05:48 +0100

    cors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const http = require('http')
 const bodyParser = require('body-parser')
@@ -5,7 +6,8 @@ const morgan = require('morgan')
 const app = express()
 const router = require('./router')
 const mongoose = require('mongoose')
-
+const cors = require('cors')
+    app.use(cors())
 mongoose.connect('mongodb://' + process.env.MONGOLAB_USER + ':' + process.env.MONGOLAB_PASS + process.env.MONGO_DB)
 // App setup
 app.use(morgan('combined'))
