da2229f5f039331a83845657ca959e8e0d4858c0
Paul de Wouters <paul@hmn.md>
2016-05-14 12:29:50 +0100

    sfsdfsdf

--- router.js
+++ router.js
@@ -2,15 +2,12 @@ const Authentication = require('./controllers/authentication')
 const Venue = require('./controllers/venue')
 const passportService = require('./services/passport')
 const passport = require('passport')
+const cors = require('cors')
 
 const requireAuth = passport.authenticate('jwt', {session:false})
 const requireSignin = passport.authenticate('local', {session:false})
 module.exports = function(app){
-    app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-    });
+    app.use(cors())
     app.get('/', requireAuth, function(req, res){
         res.send({ message: 'Super secret code is abc123' })
     })
